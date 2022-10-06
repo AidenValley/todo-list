d43d93b2c2c106fe8d1e8d5038cc1f091c4229df
AidenValley <jhyjsh953@gmail.com>
2022-10-05 20:43:59 -0700

    add button clear button remove buttons added with functions

--- todo-list/src/components/MyList.js
+++ todo-list/src/components/MyList.js
@@ -1,18 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ListItem from './ListItem';
 
 function MyList(props) {
-  let newTodo = props.theList.map((item, index) => {
-    return(
-      <ListItem task={item} key={index} />
-    )
-  });
+  const [tasks, setTasks] = useState(props.theList);
+  const [newItem, setNewItem] = useState('');
+
+  const clearList = () => {
+    console.log('clear list');
+    setTasks([]);
+  }
+
+  const handleChange = (e) => {
+    setNewItem(e.target.value);
+  }
+
+  const addItem = (e) => {
+    e.preventDefault();
+    let newList = tasks;
+    newList.push(newItem);
+    setTasks(newList);
+    setNewItem('');
+  }
+
+  const removeItem = (index) => {
+    index = parseInt(index);
+    let newList = tasks.filter((item, idx) => {
+      if(index === idx) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+    setTasks(newList);
+  }
+
+  const renderTodoItems = () => {
+    let newTodo = tasks.map((item, index) => {
+      return (
+        <ListItem task={item} key={`todo${index}`} index={index} removeItem={removeItem} />
+      )
+    });
+    return newTodo;
+  }
+
   return (
     <div>
       <h1>Things I should stop procrastinating:</h1>
+      <form onSubmit={addItem}>
+        <input type="text" value={newItem} onChange={handleChange} />
+        <button type="submit">Add To List</button>
+      </form>
       <h2>
-        {newTodo}
+        {renderTodoItems()}
       </h2>
+      <button onClick={clearList}> CLEAR </button>
     </div>
   );
 }
